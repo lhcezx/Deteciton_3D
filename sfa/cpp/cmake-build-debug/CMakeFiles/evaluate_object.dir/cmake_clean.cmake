b3ca6135cf93154068e47a0e04e0c36770a08fa1
lhc <lhcezx@gmail.com>
2021-09-19 16:57:17 +0200

    a

---
+++ sfa/cpp/cmake-build-debug/CMakeFiles/evaluate_object.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/evaluate_object.dir/evaluate_object.cpp.obj"
+  "evaluate_object.exe"
+  "evaluate_object.exe.manifest"
+  "evaluate_object.pdb"
+  "libevaluate_object.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/evaluate_object.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
