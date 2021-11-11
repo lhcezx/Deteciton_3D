e98b9bb0dd96dfa112e196ec93129caf1ffef39e
LUO <81321338+lhcezx@users.noreply.github.com>
2021-11-11 15:25:41 +0100

    Update train_config.py

--- sfa/config/train_config.py
+++ sfa/config/train_config.py
@@ -1,13 +1,3 @@
-"""
-# -*- coding: utf-8 -*-
------------------------------------------------------------------------------------
-# Author: Nguyen Mau Dung
-# DoC: 2020.08.17
-# email: nguyenmaudung93.kstn@gmail.com
------------------------------------------------------------------------------------
-# Description: The configurations of the project will be defined here
-"""
-
 import os
 import argparse
 
