56b3a7721fb20b04d19e04126a4d0d4e600860d1
LUO <81321338+lhcezx@users.noreply.github.com>
2021-11-11 08:51:51 +0100

    Update demo_front.py

--- sfa/demo_front.py
+++ sfa/demo_front.py
@@ -1,13 +1,3 @@
-"""
-# -*- coding: utf-8 -*-
------------------------------------------------------------------------------------
-# Author: Nguyen Mau Dung
-# DoC: 2020.08.17
-# email: nguyenmaudung93.kstn@gmail.com
------------------------------------------------------------------------------------
-# Description: Demonstration script for the front view only
-"""
-
 import argparse
 import sys
 import os
