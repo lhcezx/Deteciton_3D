21850e322287653df1744237d3c5e8e601f83c5d
LUO <81321338+lhcezx@users.noreply.github.com>
2021-11-11 08:51:33 +0100

    Update visualization_utils.py

--- sfa/utils/visualization_utils.py
+++ sfa/utils/visualization_utils.py
@@ -1,13 +1,3 @@
-"""
-# -*- coding: utf-8 -*-
------------------------------------------------------------------------------------
-# Author: Nguyen Mau Dung
-# DoC: 2020.08.09
-# email: nguyenmaudung93.kstn@gmail.com
------------------------------------------------------------------------------------
-# Description: The utils of the kitti dataset
-"""
-
 from __future__ import print_function
 from bdb import set_trace
 import os
